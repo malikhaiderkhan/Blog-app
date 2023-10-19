1f94f0e6769aa0e66f80f8f0f31de9842c808349
Haider <malikhaiderkhan81@gmail.com>
2023-10-19 23:28:49 +0500

    add update posts counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def updates_posts_counter
+    update(post_count: posts.count)
+  end
 end
